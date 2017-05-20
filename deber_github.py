cbe8c7727362f33d18e9dad8a6cfd54660305c95
PETER\PETER <pedro.munoz@epn.edu.ec>
2017-05-20 11:55:06 -0500

    boton_peter

--- deber_github.py
+++ deber_github.py
@@ -1,8 +1,19 @@
 def hello():
     print("Ricardo Quinzo")
 
+def peter():
+    print("Pedro Muñoz")
+
 from tkinter import *
 
 tk=Tk()
+
 btn = Button(tk, text="Ricardo", command=hello)
+btn2 = Button(tk, text= "Pedro Muñoz", command = peter)
+
 btn.pack()
+btn2.pack()
+
+
+
+
