f6833fe33d7524b2bde399ae4cab6092a0aab2f7
Daniel Acevedo <daniel.acevedo@epn.edu.ec>
2017-05-21 09:42:21 -0500

    Asginación del tercer boton Tomar en cuenta que hay que importar la funcion tk

--- deber_github.py
+++ deber_github.py
@@ -1,8 +1,11 @@
-def hello():
+﻿def hello():
     print("Ricardo Quinzo")
 
 def peter():
     print("Pedro Muñoz")
+    
+def daniel():
+    print("Daniel Acevedo")
 
 from tkinter import *
 
@@ -10,10 +13,11 @@ tk=Tk()
 
 btn = Button(tk, text="Ricardo", command=hello)
 btn2 = Button(tk, text= "Pedro Muñoz", command = peter)
+btn3 = Button(tk, text= "Daniel", command = peter)
 
 btn.pack()
 btn2.pack()
-
+btn3.pack()
 
 
 
