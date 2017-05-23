d66727d524ab5dbe67e2dd92c522bff56090b21b
DanielFlores <danielflorestamayo@gmail.com>
2017-05-23 12:12:56 -0500

    Camnio Final

--- deber_github.py
+++ deber_github.py
@@ -10,6 +10,9 @@ def daniel():
 def estefa():
     print("Estefania Aguilar")
 
+def genaro():
+    print("Daniel Flores")
+
 from tkinter import *
 
 tk=Tk()
@@ -18,11 +21,14 @@ btn = Button(tk, text="Ricardo", command=hello)
 btn2 = Button(tk, text= "Pedro Muñoz", command = peter)
 btn3 = Button(tk, text= "Daniel", command = daniel)
 btn4 = Button(tk, text= "Estefania", command = estefa)
+btn5 = Button(tk, text= "Daniel Flores", command = genaro)
 
 btn.pack()
 btn2.pack()
 btn3.pack()
 btn4.pack()
+btn5.pack()
+
 
 
 
